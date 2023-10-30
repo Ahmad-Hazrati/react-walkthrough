a6da969733db7fdb6ac4f6eb185f151e49f75e35
Ahmad Hazrati <hazrati.ahmad@gmail.com>
2023-10-30 19:45:50 +0000

    Add previous State handler to my-app

--- my-app/src/components/StatefulGreetingWithPrevState.js
+++ my-app/src/components/StatefulGreetingWithPrevState.js
@@ -12,25 +12,24 @@ class StatefulGreetingWithPrevState extends React.Component {
     }
 
     handleClick() {
-        this.setState(
-            {
-                introduction: this.state.introduction === "Hello" ? "Goodbye" : "Hello",
-                buttonText: this.state.buttonText === "Exit" ? "Enter" : "Exit",
-            },
-            () => {
-                console.log('new state', this.state.introduction);
-                console.log('new state', this.state.buttonText);
-            }
-        );
-        console.log(this.state.introduction);
-        console.log(this.state.buttonText);
+        this.setState((prevState, prevProps) => {
+            console.log('Previous State:', prevState)
+            console.log('Previous Props:', prevProps)
+            return {
+                introduction: prevState.introduction === "Hello" ? "Goodbye": "Hello",
+                buttonText: prevState.buttonText === "Exit" ? "Enter" : "Exit"
+            };
+        });
     }
 
     increment() {
-        this.setState({
-            count: this.state.count + 1
-        })
-        console.log(this.state.count)
+        this.setState((prevState, prevProps) => {
+            console.log('Previous State:', prevState)
+            console.log('Previous Props:', prevProps)
+        return {
+            count: prevState.count + 1,
+        };
+    });
     }
 
     incrementFive() {
@@ -46,13 +45,11 @@ class StatefulGreetingWithPrevState extends React.Component {
             <div>
                 <h1>{this.state.introduction}, {this.props.name}! {this.props.greeting}</h1>
                 <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
-                <button onClick={() => this.incrementFive()} > Increment Five</button>
+                <button onClick={() => this.increment()} > Increment</button>
                 <p>You've clicked {this.state.count} times</p>
             </div >
-        )
-
+        );
     }
-
 }
 
-export default StatefulGreetingWithPrevState
+export default StatefulGreetingWithPrevState;
